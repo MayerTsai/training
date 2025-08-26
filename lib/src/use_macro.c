515dbd393d43f07f60f7dd379e9002b8d031c52d
MayerTsai <mayer.tsai@gmail.com>
2025-08-26 14:17:16 +0800

    formatter

--- lib/src/use_macro.c
+++ lib/src/use_macro.c
@@ -17,7 +17,7 @@ int macro_test(void)
   printf("my_add is: %d\n", my_add(5, 10));
   printf("my_max macro: %d\n", my_max(10, 5));
 
-  printf("my_string macro: %s\n", my_string1("string1"));
+  printf("my_string1 macro: %s\n", my_string1("string1"));
   printf("my_string2 macro: %s\n", my_string2(string2));
   printf("my_string3 macro: %s\n", my_string3(string3));
   printf("my_string4 macro: %s\n", my_string4(string4));
