ed75b0d8597ce32eea30f3f38d7fbd7c94f93487
MayerTsai <mayer.tsai@gmail.com>
2025-08-26 13:50:07 +0800

    revise the ## macro example

--- lib/src/use_macro.c
+++ lib/src/use_macro.c
@@ -2,21 +2,24 @@
 #include "use_macro.h"
 #include <stdio.h>
 
-#define my_string(x) #x
-#define my_string2(x, y) #x " " #y
-#define my_add(x, y) (x + y)
+#define my_add(x, y) ((x) + (y))
 #define my_max(op1, op2) ((op1 > op2) ? (op1) : (op2))
-#define my_string3(x) my_##x
 
-// Call a function from my_functions.h
+#define my_string1(x) x
+#define my_string2(x) #x
+#define my_string3(x) my_string2(x)
+#define my_string4(x) my__##x
+#define my__string4 "string4"
+
+// Demonstrate various C preprocessor macros.
 int macro_test(void)
 {
   printf("my_add is: %d\n", my_add(5, 10));
-  printf("my_string macro: %s\n", my_string(Hello world));
-  printf("my_string2 macro: %s\n", my_string2(Hello, world));
-  printf("my_add macro: %d\n", my_add(5, 10));
   printf("my_max macro: %d\n", my_max(10, 5));
-  char *my_string3(name) = "John"; // char *my_name = "John";
-  printf("my_string3 macro: %s\n", my_name);
+
+  printf("my_string macro: %s\n", my_string1("string1"));
+  printf("my_string2 macro: %s\n", my_string2(string2));
+  printf("my_string3 macro: %s\n", my_string3(string3));
+  printf("my_string4 macro: %s\n", my_string4(string4));
   return 0;
 }
